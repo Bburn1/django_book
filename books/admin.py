e89041b9219fde5ddcbf58d2f40a8ecbd126198e
bburn1 <bogdanburn123@gmail.com>
2021-11-05 14:55:05 +0200

    starting customization admin-panel

--- books/admin.py
+++ books/admin.py
@@ -1,12 +1,23 @@
 from django.contrib import admin
 from .models import Category, Genre, Book, BookShots, Avtor, RatingStar, Rating, Reviews, Publishing, Country
 
-# class CategoryAdmin(admin.ModelAdmin):
-#
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "url")
+    list_display_links = ("name", )
 
-admin.site.register(Category)
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "category", "url", "draft")
+    list_filter = ("category", "draft", "year", )
+    search_fields = ("title", )
+
+
+# admin.site.register(Category, CategoryAdmin)
+
 admin.site.register(Genre)
-admin.site.register(Book)
+# admin.site.register(Book)
 admin.site.register(BookShots)
 admin.site.register(Avtor)
 admin.site.register(Rating)
