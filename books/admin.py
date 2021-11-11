d679401b23487d48619f1ec98e9a48f3bd373473
bburn1 <bogdanburn123@gmail.com>
2021-11-11 16:25:53 +0200

    Admin panel, сkeditor, youtube cideo

--- books/admin.py
+++ books/admin.py
@@ -1,5 +1,26 @@
+from django import forms
 from django.contrib import admin
+
+from django.utils.safestring import mark_safe
+
+
 from .models import Category, Genre, Book, BookShots, Avtor, RatingStar, Rating, Reviews, Publishing, Country
+
+
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+
+
+
+class BookAdminForm(forms.ModelForm):
+    description = forms.CharField(label="Описание", widget=CKEditorUploadingWidget())
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+
+
+
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
@@ -8,11 +29,14 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display_links = ("name", )
 
 
+
 class ReviewInline(admin.TabularInline):
     """Отзывы на странице"""
     model = Reviews
     extra = 0
     readonly_fields = ("name", "email",)
+
+
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
@@ -24,12 +48,17 @@ class BookAdmin(admin.ModelAdmin):
     save_on_top = True
     save_as = True
     list_editable = ("draft", )
+    form = BookAdminForm
+    readonly_fields = ("get_image", )
     fieldsets = (
         (None, {
             "fields": (("title", ), )
         }),
         (None, {
-            "fields": (("description", "poster",),)
+            "fields": (("description", ),)
+        }),
+        (None, {
+            "fields": (("poster", "get_image",),)
         }),
         (None, {
             "fields": (("countrys", "language", ),)
@@ -46,11 +75,13 @@ class BookAdmin(admin.ModelAdmin):
         ("Options", {
             "fields": (("url", "draft",),)
         }),
-
-
-
-
     )
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.poster.url} width="100" height="100"')
+
+    get_image.short_description = "Постер"
+
+
 
 @admin.register(Reviews)
 class ReviewAdmin(admin.ModelAdmin):
@@ -58,34 +89,51 @@ class ReviewAdmin(admin.ModelAdmin):
     list_display = ("name", "email", "parent", "book", "id")
     readonly_fields = ("name", "email", )
 
+
+
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
     """Жанры"""
     list_display = ("name", "url")
 
+
+
 @admin.register(Avtor)
 class ActorAdmin(admin.ModelAdmin):
     """Авторы"""
-    list_display = ("name", "age")
+    list_display = ("name", "age", "get_image", )
+    readonly_fields = ("get_image", )
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="50" height="60"')
+
+    get_image.short_description = "Изображение"
+
+
+
 
 @admin.register(Rating)
 class RatingAdmin(admin.ModelAdmin):
     """Рейтинг"""
     list_display = ("star", "ip")
 
+
+
 @admin.register(BookShots)
 class BookShotsAdmin(admin.ModelAdmin):
     """Кадры из фильма"""
     list_display = ("title", "book")
 
+
+
 @admin.register(Publishing)
 class PublishingAdmin(admin.ModelAdmin):
-
     list_display = ("name", "image", )
+
+
 
 @admin.register(Country)
 class CountryAdmin(admin.ModelAdmin):
-
     list_display = ("name", "image", )
 
 
@@ -106,5 +154,8 @@ admin.site.register(RatingStar)
 #admin.site.register(Reviews)
 #admin.site.register(Publishing)
 #admin.site.register(Country)
+
+admin.site.site_title = "Digital Library"
+admin.site.site_header = "Digital Library"
 
 
